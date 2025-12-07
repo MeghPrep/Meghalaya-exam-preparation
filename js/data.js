1374d577c6a941d84540da370a95c583c3268523
MeghPrep <cexam666@gmail.com>
2025-12-07 23:39:11 +0530

    Add QUIZ_TREE data structure for quizzes

---
+++ js/data.js
@@ -0,0 +1,62 @@
+const QUIZ_TREE = [
+  {
+    id: "1",
+    name: "Grade IV",
+    desc: "Focuses on Grade IV exam subjects, concepts, and practice.",
+    subcategories: [
+      {
+        id: "101",
+        name: "DSC Mairang",
+        desc: "District Eastern-West Khasi Hills",
+        papers: [
+          {
+            id: "DSC_MAIRANG_P1",
+            name: "Paper 1",
+            desc: "DSC Mairang Paper 1",
+            years: [2025, 2024, 2023]
+          },
+          {
+            id: "DSC_MAIRANG_P2",
+            name: "Paper 2",
+            desc: "DSC Mairang Paper 2",
+            years: [2025]
+          }
+        ]
+      },
+      {
+        id: "102",
+        name: "DSC Shillong",
+        desc: "Focuses on DSC Shillong topics",
+        papers: [
+          {
+            id: "DSC_SHILLONG_P1",
+            name: "Paper 1",
+            desc: "DSC Shillong Level 1",
+            years: [2025, 2023]
+          }
+        ]
+      }
+    ]
+  },
+
+  {
+    id: "2",
+    name: "Accounting / CCAAPA",
+    desc: "Tricky accounting MCQs for practice and preparation",
+    subcategories: [
+      {
+        id: "201",
+        name: "Paper 1",
+        desc: "Comprehensive accounting MCQs",
+        papers: [
+          {
+            id: "CCAAPA_100_MCQs",
+            name: "CCAAPA MCQs",
+            desc: "Full set of 100 tricky accounting questions",
+            years: [2025]
+          }
+        ]
+      }
+    ]
+  }
+];
